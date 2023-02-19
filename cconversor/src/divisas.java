5cd9d20431db5731d1879559ed76c1f57f2a5d4a
aceves-cl <git@acevescl.com>
2023-02-19 09:15:37 -0600

    Update 0.1.3

--- cconversor/src/divisas.java
+++ cconversor/src/divisas.java
@@ -4,5 +4,28 @@
     @version 0.1.3 17/02/2023
 */
 public class divisas extends datos{
-    
+
+    protected double valorEntrada, valorSalida;
+    protected String convertirDe, convertirA;
+    protected double usdToMxn, mxnToUsd, arsToMxn, mxnToArs
+
+    public divisas(){
+
+        super();
+        valorEntrada = 0;
+        valorSalida = 0;
+        convertirDe = "";
+        convertirA = "";
+        usdToMxn = 18.36;
+        mxnToUsd = 0.054;
+        arsToMxn = 0.095;
+        mxnToArs = 10.49;
+
+    }
+
+    public void convertirDivisas(){
+
+
+
+    }
 }
